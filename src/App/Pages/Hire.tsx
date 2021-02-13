024c0232f9448489da7c56f67c8b58f572b50f5b
Ognjen Vukotic <ognjendev.3@gmail.com>
2021-02-13 15:08:34 +0100

    make page 2 component

--- src/App/Pages/Hire.tsx
+++ src/App/Pages/Hire.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import PageHeader from 'App/Components/SharedComponents/PageHeader'
 
 const Hire = () => {
     return (
         <div>
-            
+
         </div>
     )
 }
