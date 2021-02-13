a58e3207661e79c373f3cfa0e8b01c3535eed46b
Ognjen Vukotic <ognjendev.3@gmail.com>
2021-02-13 16:00:32 +0100

    create input field component with props

--- src/App/Components/HireComponents/LeftSide.tsx
+++ src/App/Components/HireComponents/LeftSide.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import InputField from 'App/Components/HireComponents/LeftSideComponents/InputField'
 
 const LeftSide = () => {
     return (
-        <div className="border w-full min-h-screen">
-            yo
-        </div>
+        <fieldset className="border w-full min-h-screen rounded-md mx-6 mb-6 mt-5">
+            <legend className="pb-1 m-auto"><p className="mx-1 font-bold text-sm text-gray-700">LET'S START</p></legend>
+
+            <div>
+                <InputField />
+                <InputField />
+            </div>
+
+        </fieldset>
     )
 }
 
--- src/App/Components/HireComponents/RightSide.tsx
+++ src/App/Components/HireComponents/RightSide.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const RightSide = () => {
     return (
-        <div className="border w-2/5 min-h-full">
+        <div className="border w-3/5 min-h-full">
             yo
         </div>
     )
