98419a476e35411fa6af71cb7537e1febf60c3e8
Ognjen Vukotic <ognjendev.3@gmail.com>
2021-02-13 14:58:51 +0100

    add absolute paths to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import GetHired from '../src/App/Pages/GetHired'
+import 'App/App.css';
+import GetHired from 'App/Pages/GetHired'
+import Hire from 'App/Pages/Hire'
 
 function App() {
   return (
