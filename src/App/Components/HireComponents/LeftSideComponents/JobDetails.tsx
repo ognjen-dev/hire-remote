b908edc548ca05af12e638d191f8d5213f9cbcd0
Ognjen Vukotic <ognjendev.3@gmail.com>
2021-02-15 00:41:51 +0100

    add rich text editor

--- src/App/Components/HireComponents/LeftSideComponents/JobDetails.tsx
+++ src/App/Components/HireComponents/LeftSideComponents/JobDetails.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import SalaryRange from 'App/Components/HireComponents/LeftSideComponents/SalaryRange'
+import RichText from 'App/Components/HireComponents/LeftSideComponents/RichText'
+import InputField from 'App/Components/HireComponents/LeftSideComponents/InputField'
 
 const JobDetails = () => {
     return (
@@ -9,6 +11,14 @@ const JobDetails = () => {
                 <button className="mt-3 text-gray-500 lg:px-10 lg:py-14 rounded-md border border-gray-300 bg-gray-50">💾 Upload</button>
             </div>
             <SalaryRange desc="Not required but HIGHLY recommended, because Google does NOT index jobs without salary data! Write it preferrably in US DOLLARS PER YEAR, like $75,000. If you pay hourly, or monthly, please convert to annual equivalent yourself (hourly rate * 8h * 22d * 12mo OR monthly salary * 12mo). Our robot can also convert it but it's not perfect and it might make a mistake if you enter it differently than annual. If not sure, write an indication of the salary." />
+            <RichText title="JOB DESCRIPTION* (READ HERE HOW TO USE MARKDOWN)" />
+            <RichText title="HOW TO APPLY?" />
+            <div className="mt-5">
+                <InputField header="APPLY URL*" desc="Apply URLs with a form an applicant can fill out generally receive a lot more applicants than having people apply by email (below). A good platform to have applicants apply on is Lever.co (not affiliated)." placeholder="" />
+            </div>
+            <div className="mt-5">
+                <InputField header="APPLY EMAIL*" desc="This email is public, the Apply button links to it if you do not supply an Apply URL above." placeholder="" />
+            </div>
         </div>
     )
 }
---
+++ src/App/Components/HireComponents/LeftSideComponents/RichText.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+// @ts-ignore
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+
+// @ts-ignore
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+interface RichProps {
+    title:string
+}
+
+const RichText = ({ title }:RichProps) => {
+    return (
+        <div>
+            <p className="ml-5 mt-6 font-bold text-sm text-gray-700">{title}</p>
+            <div className="px-4 pt-4">
+                <CKEditor
+                    editor={ClassicEditor}
+                    data="<p></p><p></p><p></p><p></p>"
+                    // @ts-ignore
+                    onReady={editor => {
+                        // You can store the "editor" and use when it is needed.
+                        console.log('Editor is ready to use!', editor);
+                    }}
+                    // @ts-ignore
+                    onChange={(event, editor) => {
+                        const data = editor.getData();
+                        console.log({ event, editor, data });
+                    }}
+                    // @ts-ignore
+                    onBlur={(event, editor) => {
+                        console.log('Blur.', editor);
+                    }}
+                    // @ts-ignore
+                    onFocus={(event, editor) => {
+                        console.log('Focus.', editor);
+                    }}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default RichText
