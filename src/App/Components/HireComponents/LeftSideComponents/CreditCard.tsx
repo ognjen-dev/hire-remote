4ac36ff802d6eafc5b92f51baee4ed6d25ce6e2c
Ognjen Vukotic <ognjendev.3@gmail.com>
2021-02-16 00:07:45 +0100

    add responsiveness to left side of hire page

--- src/App/Components/HireComponents/LeftSideComponents/CreditCard.tsx
+++ src/App/Components/HireComponents/LeftSideComponents/CreditCard.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const CreditCard = () => {
     return (
-        <div className="lg:ml-2 lg: mx-2 lg:flex lg:flex-row flex flex-col justify-center lg:justify-between items-center bg-gray-50">
-            <div className="lg:flex lg:flex-row flex flex-col items-center justify-center border rounded-md bg-gray-50 block flex-1 mx-3 h-12 focus:ring-indigo-500 mt-3 h-9 focus:border-indigo-500 min-w-0 rounded-none rounded-r-md sm:text-sm border-gray-300">
+        <div className="lg:ml-2 lg: mx-2 lg:flex lg:flex-row flex flex-col justify-center lg:justify-between items-center lg:pr-12">
+            <div className="lg:flex lg:flex-row flex flex-col items-center justify-center border rounded-md block flex-1 mx-3 h-12 focus:ring-indigo-500 mt-3 h-9 focus:border-indigo-500 min-w-0 rounded-none rounded-r-md sm:text-sm border-gray-300">
                 <p>💳<input type="text" className="mr-2 lg:py-0 py-1" placeholder="Card Number" /></p>
                 <input type="text" className="ml-3 mt-1 lg:ml-0 lg:mt-0 lg:py-0 py-1" placeholder="MM/YY CVC" />
             </div>
--- src/App/Components/HireComponents/RightSideComponents/SmallAd.tsx
+++ src/App/Components/HireComponents/RightSideComponents/SmallAd.tsx
@@ -8,10 +8,10 @@ interface SmallAdProps {
 
 const SmallAd = ({ row1, row2, row3 }:SmallAdProps) => {
     return (
-        <div className="border-b py-4 w-full">
+        <div className="border-b py-4 lg:w-full w-full">
             <div className="flex lg:flex flex-col lg:flex-col items-center py-3">
                 <p className="text-l">{row1}</p>
-                <p className="font-bold text-2xl my-2 leading-5">{row2}</p>
+                <p className="lg:font-medium font-medium text-2xl text-center my-2 leading-5">{row2}</p>
                 <p className="text-l">{row3}</p>
             </div>
         </div>
