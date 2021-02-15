e8fdf33a4377aebbf08fe881da47032228c156db
Ognjen Vukotic <ognjendev.3@gmail.com>
2021-02-15 17:51:26 +0100

    finish sticky footer

--- src/App/Components/HireComponents/RightSideComponents/StickyFooter.tsx
+++ src/App/Components/HireComponents/RightSideComponents/StickyFooter.tsx
@@ -4,13 +4,13 @@ import StickyRight from 'App/Components/HireComponents/RightSideComponents/Stick
 
 const StickyFooter = () => {
     return (
-        <div className="flex flex-row border-t">
+        <div className="flex flex-row border-t h-14 fixed left-0 bottom-10 bg-white w-full justify-center">
 
-            <div className="w-full md:block hidden">
+            <div className="w-full lg:block hidden">
                 <StickyLeft />
             </div>
 
-            <div className="lg:w-5/12 w-full lg:pl-14">
+            <div className="lg:w-5/12 w-full bg-white lg:pl-14">
                 <StickyRight />
             </div>
 
--- src/App/Components/HireComponents/RightSideComponents/StickyLeft.tsx
+++ src/App/Components/HireComponents/RightSideComponents/StickyLeft.tsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 const StickyLeft = () => {
     return (
-        <div className="h-24">
+        <div className="h-24 flex flex-row justify-between items-center bg-white">
+            <div>
+                <img src="https://cdn3.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-twitch-square2-512.png" alt="yo" className="bg-white ml-10 w-11 h-11"/>
+            </div>
+            <div className="-ml-32">
+                <p className="font-bold">Position</p>
+                <p>Company</p>
+            </div>
+            <div className="flex flex-wrap">
+                <button><p className="font-mono px-1 mx-0.5 my-0.5 lg:mx-1 lg:my-0.5 lg:border-2 font-semibold border-2 text-xs border-black hover:bg-black hover:text-white rounded-md py-0.5 w-max">TAG</p></button>
+                <button><p className="font-mono px-1 mx-0.5 my-0.5 lg:mx-1 lg:my-0.5 lg:border-2 font-semibold border-2 text-xs border-black hover:bg-black hover:text-white rounded-md py-0.5 w-max">TAG</p></button>
+                <button><p className="font-mono px-1 mx-0.5 my-0.5 lg:mx-1 lg:my-0.5 lg:border-2 font-semibold border-2 text-xs border-black hover:bg-black hover:text-white rounded-md py-0.5 w-max">TAG</p></button>
+            </div>
             
+            <div>
+                <p className="opacity-40 border border-black text-black border-dashed text-medium font-semibold rounded-md px-12 py-2">
+                    Apply 
+                </p>
+            </div>
         </div>
     )
 }
