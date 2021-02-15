b45d5a2f729e3ac60e495216f88b0a2c4dcd2cb4
Ognjen Vukotic <ognjendev.3@gmail.com>
2021-02-15 01:47:04 +0100

    stylize rich text editors

--- src/App/Components/HireComponents/LeftSide.tsx
+++ src/App/Components/HireComponents/LeftSide.tsx
@@ -30,7 +30,6 @@ const LeftSide = () => {
                 <fieldset className="border w-full h-max rounded-md lg:mx-6 pb-6 mb-6 mt-5">
                     <legend className="pb-1 m-auto"><p className="mx-1 font-bold text-sm text-gray-700">COMPANY</p></legend>
                     <Company />
-                    YOYOYOYOO
                 </fieldset>
             </>
         </div>
--- src/App/Components/HireComponents/LeftSideComponents/PayLater.tsx
+++ src/App/Components/HireComponents/LeftSideComponents/PayLater.tsx
@@ -4,18 +4,21 @@ import React from 'react'
 
 const PayLater = () => {
     return (
-        <div>
-            <div className="px-5 pt-2">
-                <p className="text-sm font-bold text-gray-600">
-                    PAY LATER
-                </p>
-                <input type="checkbox" className="border rounded-lg flex-1 block w-full focus:ring-indigo-500 mt-3 h-9 focus:border-indigo-500 min-w-0 rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder={`${placeholder}`} />
-                    <p className="lg:ml-1 text-md mt-1 text-black">
-                        I'd like to pay later
-                    </p>
+        <div className="ml-5 mt-5">
+            <p className="text-sm font-bold text-gray-600">
+                PAY LATER
+            </p>
+            <div className="flex flex-row items-center my-4 hover:opacity-70">
+                <input className="form-checkbox lg:h-5 lg:w-5 text-blue-600" type="checkbox" />
+                <label className="flex flex-row">
+                    <p className="ml-2 lg:text-md text-sm">I would like to pay later.</p>
+                </label>
             </div>
+            <p className="lg:ml-0 text-xs -mt-3 text-gray-400">
+            Need to get approval for this payment? Or send the invoice to your finance department first? No problem, we'll save your job post and send you (and your finance department below) a payment link. Once it's paid we immediately publish it!
+            </p>
         </div>
     )
 }
 
-export default InputField
+export default PayLater
